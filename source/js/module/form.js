21085a4bd16bee432f13b9fb890961ac570ab96b
ravelin9 <kvist1495@gmail.com>
2023-05-13 21:11:15 +0500

    add README.md

--- source/js/module/form.js
+++ source/js/module/form.js
@@ -1,6 +1,10 @@
-const onEscKey = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
-const applicantForm = document.getElementById('modal__form');
+const form = document.getElementById('modal__form');
+const nameInput = document.getElementById('modal__name');
+const emailInput = document.getElementById('modal__email');
+const messageInput = document.getElementById('modal__message');
+const submitButton = document.getElementById('modal__submit');
 
+const onEscKey = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
 
 const onClickAndKeydown = (messageType) => {
     messageType.addEventListener('click', () => {
@@ -34,28 +38,17 @@ const onShowPopupError = () => {
 };
 
 
-const form = document.getElementById('modal__form');
-const nameInput = document.getElementById('modal__name');
-const emailInput = document.getElementById('modal__email');
-const messageInput = document.getElementById('modal__message');
-const submitButton = document.getElementById('modal__submit');
-
-// Функция для проверки валидности email
 const isEmailValid = (email) => {
     return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
 }
 
-// Функция для проверки валидности поля "Имя"
 const isNameValid = (name) => {
     return name.trim().length > 0;
 }
 
-// Функция для проверки валидности поля "Сообщение"
 const isMessageValid = (message) => {
     return message.trim().length > 0;
 }
-
-// Функция для обновления состояния кнопки "Отправить"
 const updateSubmitButtonState = () => {
     if (isNameValid(nameInput.value) && isEmailValid(emailInput.value) && isMessageValid(messageInput.value)) {
         submitButton.removeAttribute('disabled');
@@ -65,7 +58,6 @@ const updateSubmitButtonState = () => {
 };
 
 
-// Обновляем состояние кнопки при изменении полей формы
 nameInput.addEventListener('input', updateSubmitButtonState);
 emailInput.addEventListener('input', updateSubmitButtonState);
 messageInput.addEventListener('input', updateSubmitButtonState);
@@ -84,7 +76,6 @@ messageInput.addEventListener('blur', () => {
 });
 
 
-// Обрабатываем отправку формы
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
@@ -99,7 +90,6 @@ form.addEventListener('submit', (event) => {
         body: formData,
     })
         .then(response => {
-            // Удаляем лоадер в случае успешного ответа
             loader.remove();
             if (response.ok) {
                 onShowPopupSuccess();
@@ -108,7 +98,6 @@ form.addEventListener('submit', (event) => {
             }
         })
         .catch(error => {
-            // Удаляем лоадер в случае ошибки
             loader.remove();
             onShowPopupError();
         });
